2467cbdd183512b8cfc1d9c50cbb90ade7de60de
Michal Kvasničák <michal.kvasnicak@gmail.com>
2024-06-24 14:48:18 +0200

    chore: new repo who dis

---
+++ components/PrivyView.tsx
@@ -0,0 +1,7 @@
+import { usePrivy  } from '@privy-io/expo';
+
+console.log(usePrivy);
+
+export function PrivyView() {
+  return null;
+}
---
+++ components/PrivyView.web.tsx
@@ -0,0 +1,8 @@
+// just importing this causes issues with the build
+import { usePrivy } from '@privy-io/react-auth';
+
+console.log(usePrivy);
+
+export function PrivyView() {
+  return null;
+}
